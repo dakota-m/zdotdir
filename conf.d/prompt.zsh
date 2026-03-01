a990d493fefad59a6835771f1121ea946d082c02
Dakota Martin <dakota.martin1232@gmail.com>
2026-03-01 14:38:45 -0700

    testing rapid prompt starship

--- conf.d/prompt.zsh
+++ conf.d/prompt.zsh
@@ -6,4 +6,10 @@
 setopt prompt_subst transient_rprompt
 # autoload -Uz promptinit && promptinit
 # prompt p10k mmc
-source $ZDOTDIR/.p10k.zsh
+# source $ZDOTDIR/.p10k.zsh
+source $ZDOTDIR/lib/rapid-prompt.zsh
+rapid-prompt starship
+
+
+#   source /path/to/rapid-prompt.zsh
+#   rapid_prompt <theme> [theme-args...]
